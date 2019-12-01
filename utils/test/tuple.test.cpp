afd52e42c208df938fc04dccf5ec185e408c2ec4
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2019-12-01 12:20:25 +0100

    corrected tuple utils test

--- utils/test/tuple.test.cpp
+++ utils/test/tuple.test.cpp
@@ -1,9 +1,10 @@
 #include "doctest/doctest.h"
 #include "std_e/utils/tuple.hpp"
 
-
-using vector;
-using tuple;
+#include <vector>
+#include <algorithm>
+using std::vector;
+using std::tuple;
 
 struct S1{
   int i;
@@ -22,7 +23,7 @@ TEST_CASE("tuple for_each")
     tuple<S1, S2, S2> t = {S1{1}, S2{2}, S2{3}};
 
     vector<int> v;
-    for_each(t, [&](auto& t){ v.push_back(t.i); });
+    std_e::for_each(t, [&](auto& t){ v.push_back(t.i); });
 
     vector<int> v_expected = {1, 2, 3};
     CHECK( v == v_expected );
@@ -32,8 +33,8 @@ TEST_CASE("tuple for_each")
     tuple<vector<S1>,vector<S2>,vector<S2>> t = {{S1{3},S1{2},S1{1}} , {S2{-9}}, {S2{30}}};
 
     vector<int> v;
-    for_each(t, [&](auto& vt){
-      for_each(begin(vt), end(vt), [&](auto& t){ v.push_back(t.i); });
+    std_e::for_each(t, [&](auto& vt){
+        std::for_each(begin(vt), end(vt), [&](auto& t){ v.push_back(t.i); });
     });
 
     vector<int> v_expected = {3, 2, 1, -9, 30};
