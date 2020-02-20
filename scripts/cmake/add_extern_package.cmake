ae3fb685d9f65b27eb8fccd068005d9a50536992
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2020-02-20 18:12:17 +0100

    More cmake changes

--- scripts/cmake/add_extern_package.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(VERSION 3.12)
-
-# https://stackoverflow.com/a/52136398/1583122
-function(add_extern_package dependency)
-  if(NOT TARGET ${dependency})
-    add_subdirectory(extern/${dependency})
-  endif()
-endfunction()
