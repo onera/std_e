ac9e034158cc8e7e98cf3228349cfca4490055a2
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2020-06-23 17:29:03 +0200

    Minor

--- std_e/future/contract.hpp
+++ std_e/future/contract.hpp
@@ -5,5 +5,5 @@
 #ifdef NDEBUG
 #define STD_E_ASSERT(b) (void(0))
 #else
-#define STD_E_ASSERT(b) if (!(b)) throw ::std_e::assertion_failure(__FILE__,__LINE__,#b);
+#define STD_E_ASSERT(b) if (!(b)) throw ::std_e::assertion_failure(__FILE__,__LINE__,#b)
 #endif
