4419243b50efe6f632e8e849ff1eb7c8a6486704
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-03-10 23:37:29 +0100

    First adjacency_graph_base impl

--- std_e/graph/adjacency_graph/adjacency_graph.hpp
+++ std_e/graph/adjacency_graph/adjacency_graph.hpp
@@ -3,71 +3,129 @@
 
 #include <vector>
 #include <deque>
-//#include <ranges>
+#include <ranges>
 #include "std_e/future/contract.hpp"
 #include "std_e/iterator/iterator_range.hpp"
 #include "std_e/utils/dereferencing_range.hpp"
 #include "std_e/algorithm/iterator.hpp"
+#include "std_e/graph/adjacency_graph/range_of_ranges.hpp"
 
 
-namespace graph {
-
-// ror_adjacency_list {
-// ror means range of ranges
-template<template<class> class Node_range, template<class> class Adj_range = Node_range, class Int = int>
-using ror_adjacency_list = Node_range<Adj_range<int>>;
-
-/// traits {
-//// index_type {
-template<class adjacency_list_type>
-class index_type__impl;
-
-template<template<class> class NR, template<class> class AR, class Int>
-class index_type__impl<ror_adjacency_list<NR,AR,Int>> {
-  using type = Int;
-}
-template<class adjacency_list_type>
-using index_type = typename index_type__impl<adjacency_list_type>::type;
-//// index_type }
-
-//// adjacency_index_range_type {
-template<class adjacency_list_type>
-class adjacency_index_range_type__impl;
-
-template<template<class> class NR, template<class> class AR, class Int>
-class adjacency_index_range_type__impl<ror_adjacency_list<NR,AR,Int>> {
-  using type = AR<Int>;
-}
-template<class adjacency_list_type>
-using adjacency_index_range_type = typename adjacency_index_range_type__impl<adjacency_list_type>::type;
-//// adjacency_index_range_type }
-
-/// traits }
-// ror_adjacency_list }
+namespace std_e {
 
 
-template<class NT, class adj_list_type = ror_adjacency_list<std::vector>>
-class adjacency_graph {
+using default_adjacency_index_list_type = range_of_ranges<int,std::vector>;
+
+
+template<class NT, class ET, class adj_list_type>
+class adjacency_graph_base {
+  private:
+  // types
+    using node_range_type = range_from_outer<adj_list_type,NT>;
+    using edge_range_type = range_of_ranges_from<adj_list_type,ET>;
+  // data
+    adj_list_type adj_list;
+    node_range_type nodes;
+    edge_range_type edges;
   public:
-    using this_type = adjacency_graph<NT,adj_list_type>;
+    // Class invariant: adj_list.size() == nodes.size() == edges.size()
+    // Class invariant: adj_list[i].size() == edges[i].size() for all i in [0,adj_list.size())
+    constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
+    constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
+    constexpr auto nodes()                ->       node_range_type& { return nodes   ; }
+    constexpr auto nodes()          const -> const node_range_type& { return nodes   ; }
+    constexpr auto edges()                ->       edge_range_type& { return edges   ; }
+    constexpr auto edges()          const -> const edge_range_type& { return edges   ; }
+};
+
+// specialization when no edge values: the edge values are then just the (origin_id,target_id) pair, and the values can't be changed
+template<class adj_list_type>
+class origin_target_view {
+  private:
+    const adj_list_type* adj_list;
+  public:
+    origin_target_view(const adj_list_type* adj_list)
+      : adj_list(adj_list)
+    {}
+
+    // TODO
+    // auto operator[i][j] -> return (i,adj_list[i][j])
+};
+
+template<class NT, class adj_list_type>
+class adjacency_graph_base<NT,void,adj_list_type> {
+  private:
+  // types
+    using node_range_type = range_from_outer<adj_list_type,NT>;
+    using edge_range_type = origin_target_view<adj_list_type>;
+  // data
+    adj_list_type adj_list;
+    node_range_type nodes;
+  public:
+    constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
+    constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
+    constexpr auto nodes()                ->       node_range_type& { return nodes   ; }
+    constexpr auto nodes()          const -> const node_range_type& { return nodes   ; }
+    constexpr auto edges()          const -> edge_range_type        { return {&adj_list}; }
+};
+
+// specialization when no node values: the node value is then just [0,N), and the values can't be changed
+template<class ET, class adj_list_type>
+class adjacency_graph_base<void,ET,adj_list_type> {
+  private:
+  // types
+    using edge_range_type = range_of_ranges_from<adj_list_type,ET>;
+  // data
+    adj_list_type adj_list;
+    edge_range_type edges;
+  public:
+    constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
+    constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
+    constexpr auto nodes()          const -> auto                   { return std::ranges::iota_view(0,adjacency_list.size()); }
+    constexpr auto edges()                ->       edge_range_type& { return edges   ; }
+    constexpr auto edges()          const -> const edge_range_type& { return edges   ; }
+};
+
+// specialization when no node and no edge
+template<class adj_list_type>
+class adjacency_graph_base<void,void,adj_list_type> {
+  private:
+  // types
+    using edge_range_type = origin_target_view<adj_list_type>;
+  // data
+    adj_list_type adj_list;
+  public:
+    constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
+    constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
+    constexpr auto nodes()          const -> auto                   { return std::ranges::iota_view(0,adjacency_list.size()); }
+    constexpr auto edges()          const -> edge_range_type        { return {&adj_list}; }
+};
+
+template<class AGT> class adjacency;
+
+template<class NT, class ET, class adj_list_type = default_adjacency_index_list_type>
+class adjacency_graph {
+  private:
+    adjacency_graph_base<NT,ET,adj_list_type> impl;
+  public:
     using node_type = NT;
+    using edge_type = ET;
+
+    using this_type = adjacency_graph<NT,ET,adj_list_type>;
     using adjacency_list_type = adj_list_type;
-    using index_type = typename adjacency_list_type::index_type;
-    //using adjacency_index_range_type = typename adjacency_list_type::adjacency_index_range_type;
-    using node_range_type = std::vector<NT>; // TODO use any container
-    template<class T0> friend class adjacency;
+    using index_type = inner_range_type<adj_list_type>;
 
     using adjacency_type = adjacency<this_type>;
     using const_adjacency_type = adjacency<const this_type>;
+    friend class adjacency<this_type>;
+    friend class adjacency<const this_type>;
 
-    // Class invariant: adj_list.size() = nodes.size()
     constexpr
     adjacency_graph() = default;
 
     constexpr auto
     size() const -> index_type {
-      STD_E_ASSERT(adj_list.size() == nodes.size());
-      return nodes.size();
+      return impl.nodes.size();
     }
 
     constexpr auto
@@ -78,9 +136,6 @@ class adjacency_graph {
     operator[](index_type i) const -> const_adjacency_type {
       return {this,i};
     }
-  private:
-    adjacency_list_type adj_list;
-    node_range_type nodes;
 };
 
 
@@ -88,7 +143,7 @@ template<class adjacency_graph_type>
 class adjacency {
   public:
     using index_type = typename adjacency_graph_type::index_type;
-    using adjacency_index_range_type = typename adjacency_graph_type::adjacency_index_range_type;
+    using inner_range_type = typename adjacency_graph_type::inner_range_type;
     using node_type = typename adjacency_graph_type::node_type;
     using node_range_type = typename adjacency_graph_type::node_range_type;
 
@@ -158,7 +213,7 @@ class adjacency_range {
 
 ////////////////////////////////
 
-template<class NT, class adj_list_type = ror_adjacency_list<std::vector>>
+template<class NT, class adj_list_type = range_of_ranges<std::vector>>
 class io_adjacency_graph
   : public io_adjacency_graph<NT,adj_list_type>
 {
@@ -177,7 +232,7 @@ enum class adj_orientation {
 };
 
 template<class io_adjacency_graph_type>
-class io_adjacency 
+class io_adjacency
   : public adjacency<io_adjacency_graph_type>
 {
   public:
@@ -392,4 +447,4 @@ redirect_entering_adjacencies(adjacency<AGT>* old, adjacency<AGT>* new_adj) -> v
 // algorithms }
 
 
-} // graph
+} // std_e
---
+++ std_e/graph/adjacency_graph/range_of_ranges.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+
+namespace std_e {
+
+
+// ror means range of ranges
+template<class T, template<class> class Outer_range, template<class> class Inner_range = Outer_range>
+using range_of_ranges = Outer_range<Inner_range<T>>;
+
+// traits {
+/// value_type {
+template<class adjacency_list_type>
+struct index_type__impl;
+
+template<class T, template<class> class OR, template<class> class IR>
+struct index_type__impl<range_of_ranges<T,OR,IR>> {
+  using type = T;
+};
+template<class adjacency_list_type>
+using value_type = typename index_type__impl<adjacency_list_type>::type;
+/// value_type }
+
+/// inner_range_type {
+template<class adjacency_list_type>
+struct inner_range_type__impl;
+
+template<class T, template<class> class OR, template<class> class IR>
+struct inner_range_type__impl<range_of_ranges<T,OR,IR>> {
+  using type = IR<T>;
+};
+template<class adjacency_list_type>
+using inner_range_type = typename inner_range_type__impl<adjacency_list_type>::type;
+/// inner_range_type }
+
+/// range_from_outer {
+template<class adjacency_list_type, class T0>
+struct range_from_outer__impl;
+
+template<class T, template<class> class OR, template<class> class IR, class T0>
+struct range_from_outer__impl<range_of_ranges<T,OR,IR>,T0> {
+  using type = OR<T0>;
+};
+template<class adjacency_list_type>
+using range_from_outer = typename range_from_outer__impl<adjacency_list_type>::type;
+/// range_from_outer }
+
+/// range_of_ranges_from {
+template<class adjacency_list_type, class T0>
+struct range_of_ranges_from__impl;
+
+template<class T, template<class> class OR, template<class> class IR, class T0>
+struct range_of_ranges_from__impl<range_of_ranges<T,OR,IR>,T0> {
+  using type = range_of_ranges<T0,OR,IR>;
+};
+template<class adjacency_list_type, class T>
+using range_of_ranges_from = typename range_of_ranges_from__impl<adjacency_list_type,T>::type;
+/// range_of_ranges_from }
+
+// traits }
+
+
+} // std_e
