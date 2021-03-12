1f2b70c8e0a161022c39a29d97dbb203cd12f5cb
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-03-12 14:34:54 +0100

    Mixin: first try

--- std_e/graph/adjacency_graph/adjacency_graph_base.hpp
+++ std_e/graph/adjacency_graph/adjacency_graph_base.hpp
@@ -7,75 +7,100 @@
 namespace std_e {
 
 
-template<class NT, class ET, class adj_list_type>
-class adjacency_graph_base {
-  private:
-  // types
-    using node_range_type = range_from_outer<adj_list_type,NT>;
-    using edge_range_type = range_of_ranges_from<adj_list_type,ET>;
-  // data
-    adj_list_type adj_list;
-    node_range_type nodes;
-    edge_range_type edges;
+// nodes {
+template<class NT, class adj_list_type>
+class adjacency_list_node_mixin {
   public:
-    // Class invariant: adj_list.size() == nodes.size() == edges.size()
-    // Class invariant: adj_list[i].size() == edges[i].size() for all i in [0,adj_list.size())
+  // type traits
+    using node_range_type = range_from_outer<adj_list_type,NT>;
+
+  // interface
+    constexpr auto size() const -> index_type { return nodes.size(); }
+    constexpr auto nodes()       ->       node_range_type& { return nodes   ; }
+    constexpr auto nodes() const -> const node_range_type& { return nodes   ; }
+  private:
+    node_range_type nodes;
+};
+
+/// specialization when no node values: the node value is then just [0,N), and the value can't be changed
+template<class adj_list_type>
+class adjacency_list_node_mixin<void,adj_list_type> {
+  public:
+    constexpr auto size() const -> index_type { return sz; }
+    constexpr auto nodes() const -> auto { return std::ranges::iota_view(0,adjacency_list.size()); }
+  private:
+    index_type sz;
+};
+// nodes }
+
+
+// connection {
+template<class ET, class adj_list_type>
+class adjacency_list_connection_mixin {
+  public:
+  // types
+    using edge_range_type = range_of_ranges_from<adj_list_type,ET>;
+  // interface
     constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
     constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
-    constexpr auto nodes()                ->       node_range_type& { return nodes   ; }
-    constexpr auto nodes()          const -> const node_range_type& { return nodes   ; }
     constexpr auto edges()                ->       edge_range_type& { return edges   ; }
     constexpr auto edges()          const -> const edge_range_type& { return edges   ; }
+  private:
+  // data
+    adj_list_type adj_list;
+    edge_range_type edges;
 };
 
 // specialization when no edge values, the edge value is then the target node, and the value can't be changed
-template<class NT, class adj_list_type>
-class adjacency_graph_base<NT,void,adj_list_type> {
+template<class adj_list_type>
+class adjacency_list_connection_mixin<void,adj_list_type> {
+  public:
+  // interface
+    constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
+    constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
+    constexpr auto edges()          const -> const adj_list_type  & { return adj_list; }
   private:
-  // types
-    using node_range_type = range_from_outer<adj_list_type,NT>;
   // data
     adj_list_type adj_list;
-    node_range_type nodes;
+};
+// connection }
+
+
+// io_connection {
+template<class ET, class adj_list_type>
+class adjacency_list_connection_mixin {
+  // types
+    using edge_range_type = range_of_ranges_from<adj_list_type,ET>;
   public:
     constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
     constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
-    constexpr auto nodes()                ->       node_range_type& { return nodes   ; }
-    constexpr auto nodes()          const -> const node_range_type& { return nodes   ; }
-    constexpr auto edges()          const -> const adj_list_type  & { return adj_list; }
-};
-
-// specialization when no node values: the node value is then just [0,N), and the value can't be changed
-template<class ET, class adj_list_type>
-class adjacency_graph_base<void,ET,adj_list_type> {
+    constexpr auto edges()                ->       edge_range_type& { return edges   ; }
+    constexpr auto edges()          const -> const edge_range_type& { return edges   ; }
   private:
-  // types
-    using edge_range_type = range_of_ranges_from<adj_list_type,ET>;
   // data
     adj_list_type adj_list;
     edge_range_type edges;
-  public:
-    constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
-    constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
-    constexpr auto nodes()          const -> auto                   { return std::ranges::iota_view(0,adjacency_list.size()); }
-    constexpr auto edges()                ->       edge_range_type& { return edges   ; }
-    constexpr auto edges()          const -> const edge_range_type& { return edges   ; }
 };
 
-// specialization when no node and no edge values
+// specialization when no edge values, the edge value is then the target node, and the value can't be changed
 template<class adj_list_type>
-class adjacency_graph_base<void,void,adj_list_type> {
-  private:
-  // types
-    using edge_range_type = origin_target_view<adj_list_type>;
-  // data
-    adj_list_type adj_list;
+class adjacency_list_connection_mixin<void,adj_list_type> {
   public:
     constexpr auto adjacency_list()       ->       adj_list_type  & { return adj_list; }
     constexpr auto adjacency_list() const -> const adj_list_type  & { return adj_list; }
-    constexpr auto nodes()          const -> auto                   { return std::ranges::iota_view(0,adjacency_list.size()); }
     constexpr auto edges()          const -> const adj_list_type  & { return adj_list; }
+  private:
+    adj_list_type adj_list;
 };
+// io_connection }
 
+template<class NT, class ET, class adj_list_type>
+class adjacency_graph_base
+  : adjacency_list_node_mixin<NT,adj_list_type>
+  : adjacency_list_connection_mixin<ET,adj_list_type>
+{
+  // Class invariant: adj_list.size() == nodes.size() == edges.size()
+  // Class invariant: adj_list[i].size() == edges[i].size() for all i in [0,adj_list.size())
+};
 
 } // std_e
