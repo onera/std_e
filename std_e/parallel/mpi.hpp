c3ccda97c89d8e8bb63ee479e59d2dd55fd146d3
Jorge Nunez <jnunezra@onera.fr>
2021-06-03 17:38:40 +0200

    [mpi all_gather] Corrected code in response to review

--- std_e/parallel/mpi.hpp
+++ std_e/parallel/mpi.hpp
@@ -1,4 +1,4 @@
-#pragma once
+in#pragma once
 
 
 #include <limits>
@@ -10,6 +10,7 @@
 #include "std_e/future/make_array.hpp"
 #include "std_e/future/contract.hpp"
 
+#include <iostream>
 
 namespace std_e {
 
@@ -88,21 +89,43 @@ all_gather(T value, T* rbuf, MPI_Comm comm) -> void {
   if (err!=0) throw mpi_exception(err,std::string("in function \"")+__func__+"\"");
 }
 
+template<class T> auto
+all_gather(T value, MPI_Comm comm) -> std::vector<T> {
+
+  int comm_size;
+  MPI_Comm_size(comm, &comm_size);
+
+  std::vector<T> data_received(comm_size);
+
+  int err = MPI_Allgather(&value, 1, to_mpi_type<T>,
+                          data_received.data(), 1, to_mpi_type<T>, comm);
+  if (err!=0) throw mpi_exception(err,std::string("in function \"")+__func__+"\"");
+
+  return data_received;
+}
 
 template<class T> auto
-all_gather(const std::vector<T> & value, T* rbuf, const std::vector<T> & recvcounts, MPI_Comm comm) -> void {
+all_gather(const std::vector<T> & value,MPI_Comm comm) -> std::vector<T> {
   
   int comm_size;
   MPI_Comm_size(comm, &comm_size);
 
-  std::vector<int> displs(comm_size);
+  int sz = value.size();
+  std::vector<int> recvcounts = std_e::all_gather(sz,comm);
   
-  for (auto i = 0; i<displs.size(); ++i)
-    displs[i] = std::accumulate(recvcounts.begin(),recvcounts.begin()+i,0);
+  int total_sz = std::reduce(recvcounts.begin(),recvcounts.end());
+  std::vector<T> data_received(total_sz);
 
-  int err = MPI_Allgatherv(value.data(), value.size(), to_mpi_type<T>, rbuf, recvcounts.data(), displs.data(), 
+  std::vector<int> displs(comm_size);
+  std::partial_sum(recvcounts.begin(),recvcounts.end(),displs.begin());
+  displs.insert(displs.begin(),0);
+  displs.pop_back();
+
+  int err = MPI_Allgatherv(value.data(), value.size(), to_mpi_type<T>, data_received.data(), recvcounts.data(), displs.data(),
                            to_mpi_type<T>, comm);
   if (err!=0) throw mpi_exception(err,std::string("in function \"")+__func__+"\"");
+
+  return data_received;
 }
 
 template<class T> auto
--- std_e/parallel/test/mpi.mpi.test.cpp
+++ std_e/parallel/test/mpi.mpi.test.cpp
@@ -23,6 +23,21 @@ MPI_TEST_CASE("all_gather",3) {
   CHECK( data_received == vector{10,5,7} );
 }
 
+MPI_TEST_CASE("all_gather_alt",3) {
+  int value;
+  if (test_rank==0) {
+    value = 10;
+  } else if (test_rank==1) {
+    value = 5;
+  } else {
+    STD_E_ASSERT(std_e::rank(test_comm)==2);
+    value = 7;
+  }
+
+  vector<int> data_received = std_e::all_gather(value,test_comm);
+
+  CHECK( data_received == vector{10,5,7} );
+}
 
 MPI_TEST_CASE("all_gather_vect",3) {
   vector<int> value;
@@ -35,15 +50,7 @@ MPI_TEST_CASE("all_gather_vect",3) {
     value = {8,9,10,11,12};
   }
 
-  int sz = value.size();
-  std::vector<int> recvcounts(3);
-
-  std_e::all_gather(sz, recvcounts.data(), test_comm);
-  int total_sz = std::accumulate(recvcounts.begin(),recvcounts.end(),0); 
-
-  std::vector<int> rdata(total_sz);
-
-  std_e::all_gather(value, rdata.data(), recvcounts, test_comm);
+  std::vector<int> rdata = std_e::all_gather(value,test_comm);
   
   CHECK(rdata == vector{1,2,3,4,5,6,7,8,9,10,11,12} );
 }
