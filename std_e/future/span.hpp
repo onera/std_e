9f900eeca3ccf0dde48802c25b17af430185393e
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-09-17 00:35:08 +0200

    span_ref

--- std_e/future/span.hpp
+++ std_e/future/span.hpp
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include "std_e/base/dynamic_size.hpp"
 #include "std_e/base/macros.hpp"
+#include "std_e/future/contract.hpp"
 #include "std_e/utils/to_string.hpp"
 
 
@@ -69,7 +70,7 @@ class span_size<dynamic_size> {
 
 
 template<class T, ptrdiff_t N=dynamic_size>
-class span : public span_size<N> {
+class span_base : public span_size<N> {
   public:
   // type traits
     using span_size_type = span_size<N>;
@@ -84,33 +85,29 @@ class span : public span_size<N> {
     using const_iterator  = const_pointer;
 
   // ctors
-    FORCE_INLINE constexpr
-    span()
-    {}
-
     // dynamic span ctor
     FORCE_INLINE constexpr explicit
-    span(T* ptr, ptrdiff_t n)
+    span_base(T* ptr, ptrdiff_t n)
       // Precondition: [ptr,ptr+N) is valid range
       : span_size_type(n)
       , ptr(ptr)
     {}
     FORCE_INLINE constexpr explicit
-    span(T* first, T* last)
+    span_base(T* first, T* last)
       // Precondition: [first,last) is valid range
       : span_size_type(last-first)
       , ptr(first)
     {}
     template<class Range, std::enable_if_t< !std::is_pointer_v<Range> , int > =0> FORCE_INLINE constexpr
     // requires Range::data() -> T*
-    span(Range&& r)
+    span_base(Range&& r)
       : span_size_type(r.size())
       , ptr(r.data())
     {}
 
     // static span ctor
     FORCE_INLINE constexpr explicit
-    span(T* ptr)
+    span_base(T* ptr)
       // Precondition: [ptr,ptr+N) is valid range
       : span_size_type()
       , ptr(ptr)
@@ -169,33 +166,68 @@ class span : public span_size<N> {
 };
 
 template<class T, ptrdiff_t N> FORCE_INLINE constexpr auto
-begin(span<T,N>& x) {
+begin(span_base<T,N>& x) {
   return x.begin();
 }
 template<class T, ptrdiff_t N> FORCE_INLINE constexpr auto
-begin(const span<T,N>& x) {
+begin(const span_base<T,N>& x) {
   return x.begin();
 }
 template<class T, ptrdiff_t N> FORCE_INLINE constexpr auto
-end(span<T,N>& x) {
+end(span_base<T,N>& x) {
   return x.end();
 }
 template<class T, ptrdiff_t N> FORCE_INLINE constexpr auto
-end(const span<T,N>& x) {
+end(const span_base<T,N>& x) {
   return x.end();
 }
 
 template<class T0, class T1, ptrdiff_t N0, ptrdiff_t N1> constexpr auto
-operator==(const span<T0,N0>& x, const span<T1,N1>& y) -> bool {
+operator==(const span_base<T0,N0>& x, const span_base<T1,N1>& y) -> bool {
   if (x.size() != y.size()) return false;
   return std::equal(x.begin(),x.end(),y.begin());
 }
 template<class T0, class T1, ptrdiff_t N0, ptrdiff_t N1> constexpr auto
-operator!=(const span<T0,N0>& x, const span<T1,N1>& y) -> bool {
+operator!=(const span_base<T0,N0>& x, const span_base<T1,N1>& y) -> bool {
   return !(x==y);
 }
 
 
+
+
+template<class T, ptrdiff_t N> FORCE_INLINE constexpr auto
+to_string(const span_base<T,N>& x) {
+  return range_to_string(x);
+}
+
+
+template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
+operator==(const span_base<T0,N>& x, const std::vector<T1,A>& y) -> bool {
+  return x==span_base<const T1>(y);
+}
+template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
+operator!=(const span_base<T0,N>& x, const std::vector<T1,A>& y) -> bool {
+  return !(x==y);
+}
+template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
+operator==(const std::vector<T1,A>& y, const span_base<T0,N>& x) -> bool {
+  return x==y;
+}
+template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
+operator!=(const std::vector<T1,A>& y, const span_base<T0,N>& x) -> bool {
+  return !(x==y);
+}
+
+
+template<class T, ptrdiff_t N=dynamic_size>
+class span : public span_base<T,N> {
+  public:
+    using base = span_base<T,N>;
+    using base::base;
+};
+
+template<class T>
+span(T*) -> span<T>; // TODO remove (makes no sense if N=dyn_size!, but used in multi_array...)
 
 
 template<ptrdiff_t N, class T> FORCE_INLINE constexpr auto
@@ -240,27 +272,30 @@ make_span_n(Contiguous_range& x, I start, I n) {
 }
 
 
-template<class T, ptrdiff_t N> FORCE_INLINE constexpr auto
-to_string(const span<T,N>& x) {
-  return range_to_string(x);
-}
+template<class T, ptrdiff_t N=dynamic_size>
+class span_ref : public span_base<T,N> {
+  public:
+    FORCE_INLINE constexpr
+    span_ref() = delete;
 
+    template<class Range> FORCE_INLINE constexpr
+    span_ref& operator=(Range&& r) {
+      STD_E_ASSERT(r.size() == this->size());
+      std::copy(r.begin(),r.end(),this->begin());
+      return *this;
+    }
 
-template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
-operator==(const span<T0,N>& x, const std::vector<T1,A>& y) -> bool {
-  return x==make_span(y);
+    using base = span_base<T,N>;
+    using base::base;
+};
+
+template<class T> FORCE_INLINE constexpr auto
+make_span_ref(T* ptr, ptrdiff_t n) {
+  return span_ref<T,dynamic_size>(ptr,n);
 }
-template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
-operator!=(const span<T0,N>& x, const std::vector<T1,A>& y) -> bool {
-  return !(x==y);
-}
-template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
-operator==(const std::vector<T1,A>& y, const span<T0,N>& x) -> bool {
-  return x==y;
-}
-template<class T0, class T1, ptrdiff_t N, class A> constexpr auto
-operator!=(const std::vector<T1,A>& y, const span<T0,N>& x) -> bool {
-  return !(x==y);
+template<class Contiguous_range> FORCE_INLINE constexpr auto
+make_span_ref(Contiguous_range& x) {
+  return make_span_ref(x.data(),x.size());
 }
 
 
@@ -271,6 +306,10 @@ namespace std {
 
 template<class T, ptrdiff_t N>
 struct tuple_size<std_e::span<T,N>> {
+  static const size_t value = N;
+};
+template<class T, ptrdiff_t N>
+struct tuple_size<std_e::span_ref<T,N>> {
   static const size_t value = N;
 };
 
--- std_e/future/test/span.test.cpp
+++ std_e/future/test/span.test.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 #include <algorithm>
 
-TEST_CASE("Span size") {
+TEST_CASE("span size") {
   constexpr auto sp1size = std_e::span_size<1>();
   constexpr auto sp2size = std_e::span_size<1>();
   CHECK( sp1size == sp2size );
 }
 
-TEST_CASE("Span constructor") {
+TEST_CASE("span constructor") {
   std::vector<double>       a1  = {0 , 1, 2, 3, 4};
   const std::vector<double> ca1 = {0 , 1, 2, 3, 4};
   std::vector<double>       a2  = {-1, 6, 3,-1,-4};
@@ -19,7 +19,7 @@ TEST_CASE("Span constructor") {
   double* pta1        = a1.data();
   const double* ptca1 = ca1.data();
 
-  SUBCASE("Span from ptr static ") {
+  SUBCASE("from ptr static ") {
     auto spc1  = std_e::make_span<3>(pta1);
     auto cspc1 = std_e::make_span<3>(ptca1);
 
@@ -44,14 +44,14 @@ TEST_CASE("Span constructor") {
     CHECK( cspc1.end() == ptca1+3 );
   }
 
-  SUBCASE("Span from ptr") {
+  SUBCASE("from ptr") {
     auto sp1 = std_e::make_span(pta1, 5);
     auto csp1 = std_e::make_span(ptca1, 5);
 
     CHECK( sp1.size()  == a1.size() );
     CHECK( csp1.size() == ca1.size() );
 
-    SUBCASE("Span from ptr with offset") {
+    SUBCASE("from ptr with offset") {
       auto spo1  = std_e::make_span(pta1, 3, 2);
       auto cspo1 = std_e::make_span(ptca1, 3, 2);
 
@@ -75,7 +75,7 @@ TEST_CASE("Span constructor") {
     }
   }
 
-  SUBCASE("Span from container") {
+  SUBCASE("from container") {
     SUBCASE("explicit conversion") {
       auto sd1  = std_e::make_span(a1);
       auto csd1 = std_e::make_span(ca1);
@@ -128,7 +128,7 @@ TEST_CASE("Span constructor") {
     }
   }
 
-  SUBCASE("Span comparison") {
+  SUBCASE("comparison") {
     auto st1   = std_e::make_span(a1);
     auto st2   = std_e::make_span(a1);
     auto stc1  = std_e::make_span(ca1);
@@ -143,5 +143,44 @@ TEST_CASE("Span constructor") {
 
     CHECK( st1 != st3   );
     CHECK( stc1 != stc3 );
+  }
+}
+
+TEST_CASE("span assignment") {
+  SUBCASE("span (view with Regular semantics)") {
+    // span is a view, i.e. it does not own its elements
+    // but it is still a value type: it has value semantics
+    std::vector<int> v0 = {0,1,2};
+    std::vector<int> v1 = {3,4,5};
+
+    // create s0, make it point to v0
+    auto s0 = std_e::make_span(v0);
+    CHECK( s0.data() == v0.data() );
+
+    s0 = v1; // s0 points to v1 now
+    CHECK( s0.data() == v1.data() );
+
+    // the assignement was shallow,
+    // that is
+    //    - s0 changed where it was pointing,
+    //    - there was no deep assignment, v0 was not changed
+    CHECK( v0 == std::vector{0,1,2} );
+  }
+
+  SUBCASE("span_ref (with Reference semantics)") {
+    // span is a real reference type
+    //   - it does not own its elements
+    //   - it is not default constructible
+    //   - assignment means that the underlying range is assigned
+    std::vector<int> v0 = {0,1,2};
+    std::vector<int> v1 = {3,4,5};
+
+    //// create s0, make it point to v0
+    auto s0 = std_e::make_span_ref(v0);
+    CHECK( s0.data() == v0.data() );
+
+    s0 = v1; // s0 still points to v0, but the elements are re-assigned
+    CHECK( s0.data() == v0.data() );
+    CHECK( v0 == std::vector{3,4,5} );
   }
 }
