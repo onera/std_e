c11580a7dcb507552ea15ef7b43dbb7cae736556
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-10-01 23:54:49 +0200

    task + graph pt. 6 (optimized comm completion busy wait with atomic.wait)

--- std_e/execution/schedule/async_comm.cpp
+++ std_e/execution/schedule/async_comm.cpp
@@ -26,9 +26,37 @@ push_to_queue_if_deps_satisfied(auto& tg, auto& node_i, auto& q, auto& graph_sta
 }
 
 auto
+wait_one_comm_to_finish(
+  auto& comm_status, auto& graph_state, auto& tg, auto& q, int& n_task_remaining,
+  std::atomic_int& n_finished_comm_atomic, int& n_finished_comm
+) -> void
+{
+  int n_flying_comm = comm_status.size();
+  STD_E_ASSERT(n_flying_comm>0);
+
+  n_finished_comm_atomic.wait(n_finished_comm,std::memory_order_relaxed);
+  for (int i=0; i<n_flying_comm; ++i) {
+    int& ii = comm_status[i].first;
+    bool done_flag = comm_status[i].second->load(std::memory_order_acquire);
+    if (done_flag) {
+      // take into account that the task is done
+      graph_state[ii] = finished;
+      ++n_finished_comm;
+      --n_task_remaining;
+      comm_status.erase(comm_status.begin()+i);
+
+      push_to_queue_if_deps_satisfied(tg,ii,q,graph_state);
+      STD_E_ASSERT(!q.empty());
+    }
+  }
+}
+
+auto
 execute_async_comm(task_graph& tg, thread_pool& comm_tp) -> void {
   STD_E_ASSERT(tg.node(0).kind()==task_kind::input);
   std::deque<std::pair<int,std::unique_ptr<std::atomic_bool>>> comm_status;
+  std::atomic_int n_finished_comm_atomic = 0;
+  int n_finished_comm = 0;
   std::vector<char> graph_state(tg.size(),not_started);
 
   std::queue<int> q; q.push(0);
@@ -44,7 +72,12 @@ execute_async_comm(task_graph& tg, thread_pool& comm_tp) -> void {
         auto& i_done_flag = comm_status.emplace_back(std::pair{i,std::make_unique<std::atomic_bool>(false)});
         auto& done_flag = *i_done_flag.second;
         graph_state[i] = started;
-        comm_tp.push_task([&t,&done_flag](){ t.execute(); done_flag.store(true,std::memory_order_release); });
+        comm_tp.push_task([&t,&done_flag,&n_finished_comm_atomic](){
+            t.execute();
+            done_flag.store(true,std::memory_order_release);
+            n_finished_comm_atomic.fetch_add(1,std::memory_order_release);
+            n_finished_comm_atomic.notify_one();
+        });
       } else {
         t.execute();
         graph_state[i] = finished;
@@ -52,30 +85,10 @@ execute_async_comm(task_graph& tg, thread_pool& comm_tp) -> void {
 
         push_to_queue_if_deps_satisfied(tg,i,q,graph_state);
       }
-    } else { // TODO optimize busy wait with condition variable
-      int n_flying_comm = comm_status.size();
-      STD_E_ASSERT(n_flying_comm>0);
-      while (true) {
-        for (int i=0; i<n_flying_comm; ++i) {
-          int& ii = comm_status[i].first;
-          bool done_flag = comm_status[i].second->load(std::memory_order_acquire);
-          if (done_flag) {
-            // take into account that the task is done
-            graph_state[ii] = finished;
-            --n_task_remaining;
-            comm_status.erase(comm_status.begin()+i);
-
-            push_to_queue_if_deps_satisfied(tg,ii,q,graph_state);
-            STD_E_ASSERT(!q.empty());
-
-            goto end_spinloop;
-          }
-        }
-      }
-      end_spinloop: ;
+    } else {
+      wait_one_comm_to_finish(comm_status,graph_state,tg,q,n_task_remaining,n_finished_comm_atomic,n_finished_comm);
     }
   }
-          //ELOG(graph_state);
 }
 
 
