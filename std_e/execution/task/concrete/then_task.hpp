onera/std_e

commit fb7086af83518fde6b2a572f22ca17d2284d0f44

task + graph pt. 27
future<T> to future<const T&> conversion.
Tasks now also store a pointer to the result (in addition to the result itself),
so that the future<const T&> can point to a T* (i.e. can have a T**).
Ugly, and only some of it can be cleaned

std_e/execution/task/concrete/then_task.hpp
@@ -65,6 +65,7 @@ class then_task {
                                // because the values are not ready (to be computed by another task)
                                // while their address is (and won't move)
     task_result_stored_type<R> result;
+    task_result_stored_type<R>* result_p;
   public:
     using result_type = R;
     static constexpr bool enable_task = true;
@@ -77,6 +78,26 @@ class then_task {
       if constexpr (std::is_reference_v<R>) {
         result = nullptr;
       }
+      result_p = &result;
+    }
+
+    then_task(const then_task& x) = delete;
+    then_task operator=(const then_task& x) = delete;
+    //then_task(then_task&& x) = delete;
+    //then_task operator=(then_task&& x) = delete;
+
+    then_task(then_task&& x)
+      : f(std::move(x.f))
+      , args(std::move(x.args))
+      , result(std::move(x.result))
+      , result_p(&result)
+    {}
+    then_task& operator=(then_task&& x) {
+      f = std::move(x.f);
+      args = std::move(x.args);
+      result = std::move(x.result);
+      result_p = &result;
+      return *this;
     }
 
     auto
@@ -97,6 +118,10 @@ class then_task {
     auto
     result_ptr() -> void* {
       return &result;
+    }
+    auto
+    result_ptr_ptr() -> void* {
+      return &result_p;
     }
 };
 

std_e/execution/future/future.hpp
@@ -26,11 +26,11 @@ struct future;
 
 
 template<class R>
-class future { // TODO make it a class (invariant: result points to tg result)
+class future<const R&> { // TODO make it a class (invariant: result points to tg result)
   public:
     static constexpr bool enable_future = true;
-    using result_type = R;
-    using result_stored_type = task_result_stored_type<R>;
+    using result_type = const R&;
+    using result_stored_type = task_result_stored_type<const R&>;
 
   public:
     future() = default;
@@ -55,10 +55,13 @@ class future { // TODO make it a class (invariant: result points to tg result)
     auto
     result() const -> result_stored_type* {
       // TODO
-      //any_task& t = tg->node(i_node);
-      //return static_cast<result_stored_type*>(t.result_ptr());
+      //return static_cast<result_stored_type*>(task().result_ptr());
       return res;
     }
+  protected:
+     auto task() -> any_task& { // TODO const
+       return tg->node(i_node);
+     }
   private:
     task_graph* tg;
     int i_node;
@@ -77,41 +80,36 @@ class future<R&> : public future<const R&> {
     using result_type = R&;
 };
 
-//template<class R>
-//class future : private task_ref_result_wrapper<R&>, private future<const R&> { // TODO make it a class (invariant: result points to tg result)
-//    static_assert(!std::is_reference_v<R>); // if is_reference, should have matched another partial specialization
-//  public:
-//    using base = future<const R&>;
-//    static constexpr bool enable_future = true;
-//    using result_type = R;
-//    using result_stored_type = R;
-//
-//    future() = default;
-//    future(task_graph* tg, int i_node, R* res)
-//      : task_ref_result_wrapper<R&>(*res)
-//      , base(tg,i_node,as_task_ref_wrap())
-//    {}
-//
-//    auto
-//    graph() const -> task_graph* {
-//      return base::graph();
-//    }
-//    auto
-//    task_id() const -> int {
-//      return base::task_id();
-//    }
-//
-//    auto
-//    result() const -> result_stored_type* {
-//      return task_ref_result_wrapper<R&>::ptr;
-//    }
-//
-//  private:
-//    auto
-//    as_task_ref_wrap() -> task_ref_result_wrapper<R&>* {
-//      return this;
-//    }
-//};
+template<class R>
+class future : public future<const R&> { // TODO make it a class (invariant: result points to tg result)
+    static_assert(!std::is_reference_v<R>); // if is_reference, should have matched another partial specialization
+  public:
+    using base = future<const R&>;
+    static constexpr bool enable_future = true;
+    using result_type = R;
+    using result_stored_type = R;
+    using result_stored_ref_type = base::result_stored_type;
+
+    future() = default;
+    future(task_graph* tg, int i_node, R* res)
+      : base(tg,i_node,static_cast<result_stored_ref_type*>(tg->node(i_node).result_ptr_ptr()))
+    {}
+
+    auto
+    graph() const -> task_graph* {
+      return base::graph();
+    }
+    auto
+    task_id() const -> int {
+      return base::task_id();
+    }
+
+    auto
+    result() -> result_stored_type* { // TODO const
+      //return task().result_ptr();
+      return static_cast<result_stored_type*>(this->task().result_ptr());
+    }
+};
 
 
 // if a future is temporary, then it has to be single_shot (i.e. its result is used only once, hence it can be moved)

std_e/execution/task/any_task.hpp
@@ -14,6 +14,7 @@ namespace detail {
 struct any_task_internal_base {
   virtual auto execute() -> void = 0;
   virtual auto result_ptr() -> void* = 0;
+  virtual auto result_ptr_ptr() -> void* = 0;
   virtual auto kind() const -> task_kind = 0;
 
   virtual ~any_task_internal_base() {}
@@ -32,6 +33,9 @@ struct any_task_internal_impl: any_task_internal_base {
     auto result_ptr() -> void* override {
       return t.result_ptr();
     }
+    auto result_ptr_ptr() -> void* override {
+      return t.result_ptr_ptr();
+    }
 
     auto kind() const -> task_kind override {
       return t.kind();
@@ -47,6 +51,7 @@ struct null_task {
   static constexpr bool enable_task = true;
   auto execute() const -> void {}
   auto result_ptr() const -> void* { return nullptr; }
+  auto result_ptr_ptr() const -> void* { return nullptr; }
   auto kind() const -> task_kind { return task_kind::no_op; }
 };
 
@@ -67,6 +72,9 @@ class any_task {
 
     auto result_ptr() -> void* {
       return impl->result_ptr();
+    }
+    auto result_ptr_ptr() -> void* {
+      return impl->result_ptr_ptr();
     }
     auto kind() -> task_kind {
       return impl->kind();

std_e/execution/task/concrete/input_task.hpp
@@ -14,6 +14,7 @@ class input_task {
   private:
     using R = std_e::remove_rvalue_reference<T>;
     task_result_stored_type<R> result;
+    task_result_stored_type<R>* result_p;
   public:
     static constexpr bool enable_task = true;
     using result_type = R;
@@ -21,12 +22,27 @@ class input_task {
     input_task(T&& x)
     requires (std::is_rvalue_reference_v<T&&>)
       : result(std::move(x))
+      , result_p(&result)
     {}
 
     input_task(T& x)
       // see const_cast explanation in "task_result_stored_type" documentation
       : result(const_cast<std::remove_cvref_t<T>*>(&x))
+      , result_p(&result)
     {}
+
+    input_task(const input_task& x) = delete;
+    input_task& operator=(const input_task& x) = delete;
+
+    input_task(input_task&& x)
+      : result(std::move(x.result))
+      , result_p(&result)
+    {}
+    input_task& operator=(input_task&& x) {
+      result = std::move(x.result);
+      result_p = &result;
+      return *this;
+    }
 
     auto
     execute() -> void {}
@@ -39,6 +55,10 @@ class input_task {
     auto
     result_ptr() -> void* {
       return &result;
+    }
+    auto
+    result_ptr_ptr() -> void* {
+      return &result_p;
     }
 };
 

std_e/parallel/struct/communication2.hpp
@@ -121,7 +121,7 @@ template<class T> concept Distributed_array = true;
 
 template<class T> auto
 // TODO future<const gather_protocol2&> (same derived class trick)
-get_from_array(future<gather_protocol2> gp, future<dist_array<T>&> a) -> future<std::vector<T>> {
+get_from_array(future<const gather_protocol2&> gp, future<dist_array<T>&> a) -> future<std::vector<T>> {
 //get_from_array(Future_of<const gather_protocol2&> auto&& gp, Future_of<Distributed_array&> auto&& a)  {//-> future<std::vector<typename decltype(a)::value_type>> {
   //using dist_array_t = typename std::decay_t<decltype(a)>::result_type;
   //using T= typename std::decay_t<dist_array_t>::value_type;

commit 0f4b9da6dca972a918da97849160ae42db590ae0

Execution: corrected bug introduced in fb7086a

std_e/execution/task/concrete/then_task.hpp
@@ -87,12 +87,14 @@ class then_task {
     //then_task operator=(then_task&& x) = delete;
 
     then_task(then_task&& x)
-      : f(std::move(x.f))
+      : kd(x.kd)
+      , f(std::move(x.f))
       , args(std::move(x.args))
       , result(std::move(x.result))
       , result_p(&result)
     {}
     then_task& operator=(then_task&& x) {
+      kd = x.kd;
       f = std::move(x.f);
       args = std::move(x.args);
       result = std::move(x.result);