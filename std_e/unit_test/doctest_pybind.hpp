3d7c5a5a36bd11355cf98941c5d0414cb02e3bc4
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2022-02-25 14:04:18 +0100

    mpi unit test + pybind: corrected bug

--- std_e/unit_test/doctest_pybind.hpp
+++ std_e/unit_test/doctest_pybind.hpp
@@ -3,6 +3,8 @@
 #include "std_e/unit_test/doctest.hpp"
 #include "std_e/utils/embed_python.hpp"
 
+// Note: This is a copy-paste of what is in doctest, the only modified part is that we throw if we don't find a python interpreter
+
 #define CREATE_PYBIND_TEST_CASE(name,func) \
   static void func(); \
   TEST_CASE(name * doctest::description("PYBIND_TEST_CASE")) { \
--- std_e/unit_test/doctest_pybind_mpi.hpp
+++ std_e/unit_test/doctest_pybind_mpi.hpp
@@ -3,16 +3,17 @@
 #include "std_e/unit_test/doctest_pybind.hpp"
 #include "std_e/unit_test/doctest_mpi.hpp"
 
+// Note: This is a copy-paste of what is in doctest, the only modified part is that we throw if we don't find a python interpreter
+// WARNING: the description has to be "MPI_TEST_CASE"
+//          because the doctest MPI console reporter searches this exact string in the description
+
 #define CREATE_PYBIND_MPI_TEST_CASE(name,nb_procs,func) \
   static void func(DOCTEST_UNUSED int test_rank, DOCTEST_UNUSED int test_nb_procs, DOCTEST_UNUSED MPI_Comm test_comm, DOCTEST_UNUSED std::integral_constant<int,nb_procs>); \
-  TEST_CASE(name * doctest::description("PYBIND_MPI_TEST_CASE")) { \
-    std_e::throw_if_no_python_interpreter(std::string("PYBIND_MPI_TEST_CASE(\"")+name+"\")"); \
+  TEST_CASE(name * doctest::description("MPI_TEST_CASE")) { \
+    std_e::throw_if_no_python_interpreter(std::string("MPI_TEST_CASE(\"")+name+"\")"); \
     doctest::execute_mpi_test_case<nb_procs>(func); \
   } \
   static void func(DOCTEST_UNUSED int test_rank, DOCTEST_UNUSED int test_nb_procs, DOCTEST_UNUSED MPI_Comm test_comm, DOCTEST_UNUSED std::integral_constant<int,nb_procs> test_nb_procs_as_int_constant)
-  // DOC: test_rank, test_nb_procs, and test_comm are available UNDER THESE SPECIFIC NAMES in the body of the unit test
-  // DOC: test_nb_procs_as_int_constant is equal to test_nb_procs, but as a compile time value
-  //          (used in CHECK-like macros to assert the checked rank exists)
 
 #define PYBIND_MPI_TEST_CASE(name,n_proc) \
   CREATE_PYBIND_MPI_TEST_CASE(name,n_proc,DOCTEST_ANONYMOUS(DOCTEST_PYBIND_MPI_FUNC))
