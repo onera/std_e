14199e08aaed81310694afe3295f801dbce4bc8c
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2024-05-16 13:27:10 +0200

    dynarray utils

--- std_e/unit_test/doctest.hpp
+++ std_e/unit_test/doctest.hpp
@@ -5,6 +5,7 @@
 #include "std_e/utils/vector.hpp"
 #include "std_e/utils/array.hpp"
 #include "std_e/future/span.hpp"
+#include "std_e/future/dynarray.hpp"
 
 
 namespace doctest {
@@ -13,6 +14,15 @@ namespace doctest {
 template<class T, class A>
 struct StringMaker<std::vector<T,A>> {
   static String convert(const std::vector<T,A>& v) {
+    using std::to_string;
+    using std_e::to_string;
+    std::string s = to_string(v);
+    return s.c_str();
+  }
+};
+template<class T>
+struct StringMaker<std_e::dynarray<T>> {
+  static String convert(const std_e::dynarray<T>& v) {
     using std::to_string;
     using std_e::to_string;
     std::string s = to_string(v);
