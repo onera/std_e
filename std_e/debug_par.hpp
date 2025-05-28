d6f9665da4f0525625c424eaa76ff07514294f24
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2025-05-28 22:03:48 +0200

    debug facilities: giving coherent names to print-debug macros

--- std_e/debug_par.hpp
+++ std_e/debug_par.hpp
@@ -9,18 +9,34 @@
 
 namespace std_e {
   template<class... Ts> auto
-  slog(MPI_Comm comm, const Ts&... xs) -> void {
+  _rank_color_msg(MPI_Comm comm, const Ts&... xs) -> std::string {
     int rk = rank(comm);
     using std::to_string;
     using std_e::to_string;
     std::string msg = (to_string(xs) + ...) + "\n";
     std::string rank_msg = to_color_string(console_color::blue,"Rank ",rk,": ") + msg;
 
+    return rank_msg;
+  }
+
+  template<class... Ts> auto
+  async_log(MPI_Comm comm, const Ts&... xs) -> void {
+    std::string rank_msg = _rank_color_msg(comm, xs...);
+    std::cout << rank_msg;
+  }
+  template<class... Ts> auto
+  sync_log(MPI_Comm comm, const Ts&... xs) -> void {
+    std::string rank_msg = _rank_color_msg(comm, xs...);
     std::cout << cat_on_rank_0(rank_msg, comm);
   }
 } // std_e
 
-#define SLOG(comm,x) std_e::slog(comm,#x," = ",x)
+#define ASYNC_LOG(comm,x) std_e::async_log(comm,#x," = ",x)
+#define SYNC_LOG (comm,x) std_e::sync_log(comm,#x," = ",x)
 
-// For lazy people
-#define CLOG(x) SLOG(comm, x)
+// For lazy people (only usable if the communicator is called `comm`)
+#define ALOG(msg) std_e::async_log(comm,msg)
+#define EALOG(x)  std_e::async_log(comm,#x," = ",x)
+
+#define SLOG(msg) std_e::sync_log(comm,msg)
+#define ESLOG(x)  std_e::sync_log(comm,#x," = ",x)
